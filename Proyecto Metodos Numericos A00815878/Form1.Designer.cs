73c413caf69ece79c783aa997b73f6a3cf70de12
Ariel Alejandro Santos Alemán <A00815878@itesm.mx>
2017-11-24 00:01:16 -0600

    soy pemdejo

--- Proyecto Metodos Numericos A00815878/Form1.Designer.cs
+++ Proyecto Metodos Numericos A00815878/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.Aceptar = new System.Windows.Forms.Button();
+            this.TamañoIncognitas = new System.Windows.Forms.Button();
             this.Secante = new System.Windows.Forms.Button();
             this.TrapezoidalCompuesta = new System.Windows.Forms.Button();
             this.TrapezoidalSimple = new System.Windows.Forms.Button();
@@ -41,8 +41,17 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TamañoMatriz = new System.Windows.Forms.Button();
+            this.Montante = new System.Windows.Forms.Button();
+            this.EliminacionGauss = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -50,6 +59,9 @@
             this.tabControl1.AccessibleName = "";
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
             this.tabControl1.Location = new System.Drawing.Point(-1, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -58,7 +70,7 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.Aceptar);
+            this.tabPage1.Controls.Add(this.TamañoIncognitas);
             this.tabPage1.Controls.Add(this.Secante);
             this.tabPage1.Controls.Add(this.TrapezoidalCompuesta);
             this.tabPage1.Controls.Add(this.TrapezoidalSimple);
@@ -76,14 +88,14 @@
             this.tabPage1.Text = "Ecuaciones";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // Aceptar
+            // TamañoIncognitas
             // 
-            this.Aceptar.Location = new System.Drawing.Point(236, 17);
-            this.Aceptar.Name = "Aceptar";
-            this.Aceptar.Size = new System.Drawing.Size(75, 23);
-            this.Aceptar.TabIndex = 9;
-            this.Aceptar.Text = "Aceptar";
-            this.Aceptar.UseVisualStyleBackColor = true;
+            this.TamañoIncognitas.Location = new System.Drawing.Point(236, 17);
+            this.TamañoIncognitas.Name = "TamañoIncognitas";
+            this.TamañoIncognitas.Size = new System.Drawing.Size(75, 23);
+            this.TamañoIncognitas.TabIndex = 9;
+            this.TamañoIncognitas.Text = "Aceptar";
+            this.TamañoIncognitas.UseVisualStyleBackColor = true;
             // 
             // Secante
             // 
@@ -166,13 +178,91 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.EliminacionGauss);
+            this.tabPage2.Controls.Add(this.Montante);
+            this.tabPage2.Controls.Add(this.TamañoMatriz);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.textBox2);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(711, 296);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Matricez";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(102, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tamaño de la matriz";
+            // 
+            // TamañoMatriz
+            // 
+            this.TamañoMatriz.Location = new System.Drawing.Point(223, 41);
+            this.TamañoMatriz.Name = "TamañoMatriz";
+            this.TamañoMatriz.Size = new System.Drawing.Size(75, 23);
+            this.TamañoMatriz.TabIndex = 2;
+            this.TamañoMatriz.Text = "Aceptar";
+            this.TamañoMatriz.UseVisualStyleBackColor = true;
+            // 
+            // Montante
+            // 
+            this.Montante.Location = new System.Drawing.Point(10, 266);
+            this.Montante.Name = "Montante";
+            this.Montante.Size = new System.Drawing.Size(75, 23);
+            this.Montante.TabIndex = 3;
+            this.Montante.Text = "Montate";
+            this.Montante.UseVisualStyleBackColor = true;
+            // 
+            // EliminacionGauss
+            // 
+            this.EliminacionGauss.Location = new System.Drawing.Point(115, 266);
+            this.EliminacionGauss.Name = "EliminacionGauss";
+            this.EliminacionGauss.Size = new System.Drawing.Size(102, 23);
+            this.EliminacionGauss.TabIndex = 4;
+            this.EliminacionGauss.Text = "Eliminacion Gauss";
+            this.EliminacionGauss.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(711, 296);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(711, 296);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(711, 296);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -185,6 +275,8 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -194,7 +286,7 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.Button Aceptar;
+        private System.Windows.Forms.Button TamañoIncognitas;
         private System.Windows.Forms.Button Secante;
         private System.Windows.Forms.Button TrapezoidalCompuesta;
         private System.Windows.Forms.Button TrapezoidalSimple;
@@ -204,6 +296,14 @@
         private System.Windows.Forms.Button NewtonRaphson;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button TamañoMatriz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button EliminacionGauss;
+        private System.Windows.Forms.Button Montante;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
     }
 }
 
